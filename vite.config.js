4df00595d1c1adc7b2d4509d5cc262c17d62c441
Viktor Kysil <kisvik2004@gmail.com>
2024-04-03 20:03:24 +0300

    Fixes for config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/calculator-vue/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
